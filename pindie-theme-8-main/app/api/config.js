238622e9ed04df0f14282172ba69df00f4c49e57
LOSOcb <m.ilyshin@yandex.ru>
2024-05-30 22:07:11 +0300

    -

--- pindie-theme-8-main/app/api/config.js
+++
@@ -1,7 +0,0 @@
-export const BASE_URL = 'https://finalbeck.nomorepartiesco.ru/admin/dashboard'
-
-export const endpoints = {
-  games: `${BASE_URL}/games`,
-  auth: `${BASE_URL}/auth/local`,
-  me: `${BASE_URL}/users/me`,
-}
